fad89a8ab07b8822ffdc3acff14e77b621d215ad
Mosh Hamedani <programmingwithmosh@gmail.com>
2021-08-30 14:39:30 -0400

    Creating API views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view()
+def product_list(request):
+    return Response('ok')
+
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
