51a804a132462680caa82df7b8b1e8026759beea
Mosh Hamedani <programmingwithmosh@gmail.com>
2021-09-02 09:11:10 -0400

    Routers

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.urls.conf import include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+urlpatterns = router.urls
