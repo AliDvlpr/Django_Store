48c922cfb46748ca91f058f27ce2d47ee635b88c
Mosh Hamedani <programmingwithmosh@gmail.com>
2021-09-03 09:45:39 -0400

    Fix the bug with reviews not filtered by product

--- store/views.py
+++ store/views.py
@@ -39,9 +39,12 @@ class CollectionViewSet(ModelViewSet):
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
