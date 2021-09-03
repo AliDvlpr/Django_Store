a2177638407facfa4773947fb765cde53f05a8aa
Mosh Hamedani <programmingwithmosh@gmail.com>
2021-09-03 10:19:38 -0400

    Filtering

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
